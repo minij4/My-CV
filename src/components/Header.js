e17b9da88906c88c5274afa9f4a48ca51ab29a14
minij4 <emilijaskirmantaite@gmail.com>
2023-01-21 19:40:51 +0200

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <ul className="nav">
+            <li>MINIJA</li>
+            <li>about</li>
+            <li>education</li>
+            <li>experience</li>
+            <li>contact</li>
+        </ul>
+    );
+}
