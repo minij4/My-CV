2a41bae5b30312a9685d1b3a482bb50dca5fab4f
minij4 <emilijaskirmantaite@gmail.com>
2023-01-22 18:18:19 +0200

    Navigation show and hide on H1 click

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import Header from "./components/Header";
 import Section from "./components/Section";
 import data from "./sections-data.js";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Animated} from "react-animated-css";
+
+
 export default function Header() {
+    const [navShow, setNavShow] = React.useState(false);
+
+    function showNav() {
+        setNavShow(prevNavShow => !prevNavShow);
+    }
+
+    const styles = {
+        paddingBottom: navShow == true ? "" : "0"
+    }
+
+    const transition = {
+        opacity: 1,
+        from: { opacity: 0 },
+        config: { mass: 10, tension: 10, friction: 10 }
+    }
+
     return (
-        <ul className="nav">
-            <li className="nav-title">Minija</li>
-            <li>about</li>
-            <li>education</li>
-            <li>experience</li>
-            <li>contact</li>
-        </ul>
+        <div>
+            <h1 className="nav-title" onClick={showNav} style={styles}>Minija</h1>
+                {navShow == true &&
+                    <Animated animationIn="fadeIn" animationOut="fadeOut" isVisible={navShow}>
+                        <ul className="nav">
+                            <li>about</li>
+                            <li>education</li>
+                            <li>experience</li>
+                            <li>contact</li>
+                        </ul>
+                    </Animated>
+                }
+        </div>
+        
     );
 }
